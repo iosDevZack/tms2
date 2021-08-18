aa1cc35e5a672c4377fdfa746373f6db54814d1d
iosDevZack <86024420+iosDevZack@users.noreply.github.com>
2021-08-18 15:57:09 +0300

    Add files via upload
    Dz 17

---
+++ lesson17 homeWork/lesson12 homeWork/lesson12 homeWork/ViewsControllers/ViewControllerSettings.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController3.swift
+//  lesson12 homeWork
+//
+//  Created by Женя  on 25.07.21.
+//
+
+import UIKit
+
+class ViewController3: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func closeButtonAction(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+
+
