5849d69736ad2a00fc778df228664b3ba5d063d0
iosDevZack <86024420+iosDevZack@users.noreply.github.com>
2021-12-05 21:36:32 +0300

    Not Finish

---
+++ SwiftUIWeatherProject/SwiftUIWeatherProject/SwiftUIWeatherProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWeatherProjectApp.swift
+//  SwiftUIWeatherProject
+//
+//  Created by Женя  on 5.12.21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWeatherProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
